c54193ebe5a6688d3c067204326aca94ce5e5de5
Pharoah Campbell <pharoahcampbell@gmail.com>
2020-09-26 02:46:37 -0400

    Initial Commit

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ShellHacksBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShellHacksBackend.WebApiApplication" Language="C#" %>
