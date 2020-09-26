8d0301fd233be58ebd752a7c8670f1e083b36ee6
Bryan Arciniega <baarciniega@cpp.edu>
2020-09-26 15:09:29 -0700

    added JSON property names needed for noSQL database

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -5,20 +5,32 @@ using System.Web;
 
 namespace ShellHacksBackend.Models
 {
+    using Newtonsoft.Json;
     public class UserModel
     {
+        [JsonProperty(PropertyName = "userid")]
         public int UserId { get; set; }
+        [JsonProperty(PropertyName = "username")]
         protected string UserName { get; private set; }
+        [JsonProperty(PropertyName = "userpass")]
         protected string UserPass { get; private set; }
+        [JsonProperty(PropertyName = "monthlyincome")]
         public double MonthlyIncome { get; set; }
+        [JsonProperty(PropertyName = "monthlyincome")]
         public double MonthlyDebt { get; set; }
+        [JsonProperty(PropertyName = "monthlyspending")]
         public double MonthlySpending { get; set; }
+        [JsonProperty(PropertyName = "monthlySaveAndInv")]
         public double MonthlySaveAndInv { get; set; }
+        [JsonProperty(PropertyName = "totalincome")]
         public double TotalIncome { get; set; }
+        [JsonProperty(PropertyName = "totalspending")]
         public double TotalSpending { get; set; }
+        [JsonProperty(PropertyName = "totaldebt")]
         public double TotalDebt { get; set; }
+        [JsonProperty(PropertyName = "totalSaveAndInv")]
         public double TotalSaveAndInv { get; set; }
-        public UserModel( int userid, string username, string userpass, double monthlyincome, double monthlydebt, double monthlyspending,
+        public UserModel(int userid, string username, string userpass, double monthlyincome, double monthlydebt, double monthlyspending,
             double monthlySaveAndInv, double totalincome, double totalspending, double totaldebt, double totalsaveandinv)
         {
             // user related info
